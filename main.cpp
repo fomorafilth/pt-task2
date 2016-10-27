21e1cf050d0066afd0a17d158e5f39a8cc050527
Anna Kolesnichenko <anna-lacrymosa@yandex.ru>
2016-10-27 15:34:49 +0300

    add new class B

--- main.cpp
+++ main.cpp
@@ -2,43 +2,28 @@
 
 using namespace std;
 
-class Base
+class A
 {
 public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
+    A() { cout << "Construct Base object\n"; }
+    ~A() { cout << "Destruct Base object\n"; }
 
-    virtual void func() const { cout << "Function func() of class Base\n"; }
 };
 
-class Child: public Base
+class B: public A
 {
 public:
-    Child() { cout << "Construct Child object\n"; }
-    ~Child() { cout << "Destruct Child object\n"; }
+    B() { cout << "Construct Child object\n"; }
+    ~B() { cout << "Destruct Child object\n"; }
 
-    void func() const { cout << "Function func() of class Child\n"; }
+    
 };
 
-void Function(const Base &obj)
-{
-    cout << "* Call method func() for object obj" << endl;
-    obj.func();
-}
 
 int main(int argc, char *argv[])
 {
-    cout << "* Create Object base" << endl;
-    Base base;
-
-    cout << "* Create Object child" << endl;
-    Child child;
-
-    cout << "* Call method func() for object base" << endl;
-    Function(base);
-
-    cout << "* Call method func() for object child" << endl;
-    Function(child);
+    cout << "* Create Object B" << endl;
+    B child;
 
     return 0;
 }
